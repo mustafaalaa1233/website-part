3880e9f219765701105633e43e8bba28037209ee
mustafaalaa1233 <105825701+mustafaalaa1233@users.noreply.github.com>
2023-07-20 14:08:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+setTimeout(function() {
+    document.getElementById("https://mohameds16.github.io/jfc/Cosmetics/images/LIPSTCKS/ZUREOLIPSTCK/IMG_4617.webp").src = "https://mohameds16.github.io/jfc/Cosmetics/images/BLUSHERS/IMG_5050.webp" ;
+}, 2000);
